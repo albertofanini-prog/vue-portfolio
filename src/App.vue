7acf0389a99cda116ff90f846f6d098cf4266428
albertofanini-prog <albertofanini@outlook.com>
2022-06-15 15:04:56 +0200

    navbar router-link

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,61 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
+  <PortfolioNavbar/>
   <router-view/>
 </template>
 
+<script>
+import PortfolioNavbar from '@/components/PortfolioNavbar.vue'
+export default {
+  components:{
+    PortfolioNavbar
+  }
+}
+</script>
+
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+ul{
+  list-style: none;
+}
+
+a{
+  text-decoration: none;
+}
+
+/* img{
+  width: 100%;
+  height: 100%;
+} */
+
+body{
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  min-height: 100vh;
+  width: 100%;
+  background-color: #1A1E23;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
 }
 
 nav {
-  padding: 30px;
+  height: 75px;
 }
 
 nav a {
---
+++ src/components/PortfolioNavbar.vue
@@ -0,0 +1,63 @@
+<template>
+    <nav class="navbar">
+        <ul class="router-link_list">
+            <router-link class="router-link_item active" to="/">home</router-link>
+            <router-link class="router-link_item" to="/about">about me</router-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'PortfolioNavbar'
+}
+</script>
+
+<style lang="scss">
+
+.navbar{
+    // border: 5px solid purple;
+    position: relative;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.navbar .line{
+    height: 2px;
+    position: absolute;
+    bottom: 0;
+    // margin: 10px 0 0 0;
+    background: #FF1847;
+}
+
+.router-link_list{
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 30px;
+    // border: 3px solid green;
+    width: 80%;
+    height: 100%;
+}
+
+.router-link_item{
+    opacity: 0.4;
+    transition: all 0.4s ease;
+    color: #FFF;
+    text-transform: uppercase;
+    display: block;
+    font-weight: 600;
+    letter-spacing: 0.2em;
+}
+
+.router-link_item:hover{
+    opacity: 0.7;
+}
+
+.router-link_item.active{
+    opacity: 1;
+}
+
+</style>
