310c3f35ba24f62c567004d206843e9416319e02
albertofanini-prog <albertofanini@outlook.com>
2022-06-16 00:26:27 +0200

    commit

--- src/components/GithubSection.vue
+++ src/components/GithubSection.vue
@@ -2,10 +2,10 @@
     <section class="github_section">
         <div class="github_title">
             <h1>GitHub | albertofanini-prog</h1>
-            <p>Visita il mio profilo GitHub cliccando <a href="https://github.com/albertofanini-prog">qui</a>.</p>
+            <p>Visita il mio profilo GitHub cliccando <a target="_blank" href="https://github.com/albertofanini-prog">qui</a>.</p>
         </div>
         <div class="card_container">
-            <h3>Cards dei progetti.</h3>
+            <h3>Alcuni dei progetti per me più significativi realizzati durante il master in Boolean.</h3>
             <ul class="card_list">
                 <li class="card">
                     <div class="card_img">
@@ -19,21 +19,103 @@
                             Carosello di immagini realizzato con HTML, CSS e JS.
                         </p>
                         <p>
-                            Visita il codice cliccando <a href="https://github.com/albertofanini-prog/js-array-carousel">qui</a>.
+                            Visita il codice cliccando <a target="_blank" href="https://github.com/albertofanini-prog/js-array-carousel">qui</a>.
                         </p>
                         <p>
-                            Visita il progetto cliccando <a href="https://albertofanini-prog.github.io/js-array-carousel/">qui</a>.
+                            Visita il progetto cliccando <a target="_blank" href="https://albertofanini-prog.github.io/js-array-carousel/">qui</a>.
+                        </p>
+                        <p class="data_p">
+                            10/02/2022
                         </p>
                     </div>
                 </li>
                 <li class="card">
-                    b
+                    <div class="card_img">
+                        <img src="../assets/projects_screens/VueToDoList.png" alt="">
+                    </div>
+                    <div class="card_text">
+                        <h4>
+                            Vue ToDoList
+                        </h4>
+                        <p>
+                            ToDoList realizzata con VueJS.
+                        </p>
+                        <p>
+                            Visita il codice cliccando <a target="_blank" href="https://github.com/albertofanini-prog/vue-todolist">qui</a>.
+                        </p>
+                        <p>
+                            Visita il progetto cliccando <a target="_blank" href="https://albertofanini-prog.github.io/vue-todolist/">qui</a>.
+                        </p>
+                        <div class="data_p">
+                            26/02/2022
+                        </div>
+                    </div>
                 </li>
                 <li class="card">
-                    b
+                    <div class="card_img">
+                        <img src="../assets/projects_screens/Boolzapp.png" alt="">
+                    </div>
+                    <div class="card_text">
+                        <h4>
+                            Boolzapp
+                        </h4>
+                        <p>
+                            Replica di Whatsapp con searchbar per il filtraggio dei contatti e risposta automatica. Realizzata con HTML, CSS, JS.
+                        </p>
+                        <p>
+                            Visita il codice cliccando <a target="_blank" href="https://github.com/albertofanini-prog/js-html-css-boolzap">qui</a>.
+                        </p>
+                        <p>
+                            Visita il progetto cliccando <a target="_blank" href="https://albertofanini-prog.github.io/js-html-css-boolzap/">qui</a>.
+                        </p>
+                        <div class="data_p">
+                            28/02/2022
+                        </div>
+                    </div>
                 </li>
                 <li class="card">
-                    b
+                    <div class="card_img">
+                        <img src="../assets/projects_screens/VueNetflix.png" alt="">
+                    </div>
+                    <div class="card_text">
+                        <h4>
+                            Netflix
+                        </h4>
+                        <p>
+                            Replica di Netflix realizzata con VueJS che, recuperando i dati da TheMovieDatabase, mostra i risultati in pagina.
+                            Tramite la searchbar è possibile ricercare i film o le serie tv scrivendo il titolo.
+                            Quando il cursore viene spostato sopra al film, verranno mostrati: il titolo originale, il titolo tradotto, la lingua originale
+                            trasformata da sigla (EN, IT...) a bandiera. La descrizione e il voto numerico trasformato da numero a stella.
+                        </p>
+                        <p>
+                            Visita il codice cliccando <a target="_blank" href="https://github.com/albertofanini-prog/vue-netflix">qui</a>.
+                        </p>
+                        <div class="data_p">
+                            28/02/2022
+                        </div>
+                    </div>
+                </li>
+                <li class="card laravel">
+                    <div class="card_img">
+                        <img src="../assets/projects_screens/VueNetflix.png" alt="">
+                    </div>
+                    <div class="card_text">
+                        <h4>
+                            Netflix
+                        </h4>
+                        <p>
+                            Replica di Netflix realizzata con VueJS che, recuperando i dati da TheMovieDatabase, mostra i risultati in pagina.
+                            Tramite la searchbar è possibile ricercare i film o le serie tv scrivendo il titolo.
+                            Quando il cursore viene spostato sopra al film, verranno mostrati: il titolo originale, il titolo tradotto, la lingua originale
+                            trasformata da sigla (EN, IT...) a bandiera. La descrizione e il voto numerico trasformato da numero a stella.
+                        </p>
+                        <p>
+                            Visita il codice cliccando <a target="_blank" href="https://github.com/albertofanini-prog/vue-netflix">qui</a>.
+                        </p>
+                        <div class="data_p">
+                            28/02/2022
+                        </div>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -48,7 +130,7 @@ export default {
 
 <style lang="scss">
 .github_section{
-    border: 5px solid orange;
+    // border: 5px solid orange;
     color: #FFF;
     display: flex;
     flex-direction: column;
@@ -62,7 +144,7 @@ export default {
     align-items: center;
     justify-content: center;
     gap: 10px;
-    border: 3px solid green;
+    // border: 3px solid green;
     width: 80%;
 }
 
@@ -85,41 +167,55 @@ export default {
 }
 
 .card_container{
-    border: 3px solid green;
-    width: 80%;
+    // border: 3px solid green;
+    width: 90%;
 }
 
 .card_container h3{
     // width: 100%;
-    text-align: left;
+    text-align: center;
     padding: 10px 0px;
 }
 
 .card_list{
-    border: 2px solid yellow;
+    // border: 2px solid yellow;
     width: 100%;
     // height: 600px;
     display: flex;
     align-items: center;
+    justify-content: center;
     flex-wrap: wrap;
     gap: 40px;
     padding: 20px 0px;
 }
 
 .card{
-    border: 1px solid red;
-    width: calc((100% / 3) - 40px);
-    height: 500px;
+    // border: 1px solid red;
+    width: calc((100% / 2) - 40px);
+    // height: 650px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     border-radius: 15px;
     overflow: hidden;
+    box-shadow: rgba(255, 255, 255, 0.15) 0px 5px 15px;
+    opacity: 0.6;
+    transition: 0.5s ease-in-out;
+}
+
+@media screen and (max-width: 880px){
+    .card{
+        width: calc(90% - 40px);
+    }
+}
+
+.card:hover{
+    opacity: 1;
 }
 
 .card_img{
     width: 100%;
-    height: 50%;
+    height: 60%;
     overflow: hidden;
 }
 
@@ -130,13 +226,37 @@ export default {
 
 .card_text{
     width: 100%;
-    height: 50%;
+    height: 40%;
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
+    gap: 10px;
+    position: relative;
+    padding: 20px 5px;
 }
+
+.card_text h4{
+    font-size: 1.6rem;
+    transition: 0.3s ease-in-out;
+}
+
+.card_text h4:hover{
+    color: #42b983;
+}
+
+.data_p{
+    position: absolute;
+    left: 2%;
+    bottom: 3%;
+    font-size: 0.7rem;
+}
+
+.laravel{
+    width: calc(90% - 40px);
+}
+
 </style>
--- src/components/HomeInfo.vue
+++ src/components/HomeInfo.vue
@@ -33,14 +33,15 @@
         </li>
       </ul>
       <p class="description">
-        Nato ad Arezzo il 3 luglio del 1998, ho sempre avuto la passione per la tecnologia e per il web development. <br>
+        <!-- Nato ad Arezzo il 3 luglio del 1998, ho sempre avuto la passione per la tecnologia. <br>
         E una passione che ho iniziato a coltivare già dalla scuola media quando, scegliendo l'indirizzo informatico, ebbi modo di iniziare a utilizzare
         le varie tecnologie di base quali HTML e JS.<br>
         Stanco della solita vita monotona, a fine novembre 2021, decido di lasciare il tanto ambito posto fisso per intraprendere il corso
         formativo da Full Stack Web Developer con Boolean Careers.<br>
         Il corso mi ha fatto capire quanto per me questo settore sia stimolante, creativo e quanto mi invoglia a finalizzare le varie tecnologie e a impararne di nuove.<br>
         <br>
-        Dopo questa breve presentazione ti do il benvenuto nel mio primo portfolio realizzato con VueJS e alcune tecnologie a lui integrabili.
+        Dopo questa breve presentazione ti do il benvenuto nel mio primo portfolio realizzato con VueJS e alcune tecnologie a lui integrabili. -->
+        Descrizione
       </p>
     </div>
   </section>
@@ -84,8 +85,8 @@ export default {
 
 @media screen and (max-width: 630px){
     .home_section_pic{
-        width: 95%;
-    justify-content: center;
+      width: 95%;
+      justify-content: center;
     }
 }
 
